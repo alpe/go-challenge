a3d631bbf64909248f2557e37a201da413e0da81
Alex Peters <alpe@users.noreply.github.com>
2015-03-24 08:14:05 +0100

    Learned: reduce number of methods to keep it simple

--- pattern_printout.go
+++ pattern_printout.go
@@ -17,16 +17,12 @@ func (p Pattern) String() string {
 	w := new(bytes.Buffer)
 	fmt.Fprintf(w, "Saved with HW Version: %s\n", p.version)
 	fmt.Fprintf(w, "Tempo: %v\n", p.tempo)
-	for _, v := range p.tracks {
-		appendTrack(w, *v)
+	for _, t := range p.tracks {
+		fmt.Fprintf(w, "(%v) %v\t", t.id, t.name)
+		appendSteps(w, t.steps)
+		w.WriteString("\n")
 	}
 	return w.String()
-}
-
-func appendTrack(w *bytes.Buffer, t Track) {
-	fmt.Fprintf(w, "(%v) %v\t", t.id, t.name)
-	appendSteps(w, t.steps)
-	w.WriteString("\n")
 }
 
 func appendSteps(w *bytes.Buffer, s Steps) {
